5ae94b91b8ee4b47ef294380cfc85fa2bc96a69e
Arthur Benfica <Arthur Benfica@PC-ARTHUR>
2023-02-13 12:37:29 -0300

    Trabalhando com exceções multiplas.

--- AulasAvancadas/src/exemplosExcecao/ExemploCatchsMultiplos.java
+++ AulasAvancadas/src/exemplosExcecao/ExemploCatchsMultiplos.java
@@ -2,47 +2,36 @@ package exemplosExcecao;
 
 import javax.swing.JOptionPane;
 
-public class ExecutavelListaDinamicaSemComentarios {
+public class ExemploCatchsMultiplos {
 	public static void main(String[] args) {
 		try {
-			Aluno aluno = null;
+			Aluno aluno = new Aluno();
+			String sexString = JOptionPane.showInputDialog("Selecione o sexo do aluno (M/F):");
+			aluno.setSexo(sexString);
+			String nomeString = JOptionPane.showInputDialog("Informe o nome do aluno:");
+			aluno.setNome(nomeString);
 			for (int pos = 1; pos <= 2; pos++) {
 				String nomeDisciplina = JOptionPane.showInputDialog("Informe a " + pos + "ª disciplina:");
 				String notaDisciplina = JOptionPane
-						.showInputDialog("Informe a nota em " + nomeDisciplina + " do aluno: ");
+						.showInputDialog("Informe a nota em " + nomeDisciplina + " do aluno " + aluno.getNome() + ": ");
 				ObjetoDisciplinaComLista discilplinaDaLista = new ObjetoDisciplinaComLista();
 				discilplinaDaLista.setDisciplina(nomeDisciplina);
 				discilplinaDaLista.setNota(Double.parseDouble(notaDisciplina));
 				aluno.getDisciplinas().add(discilplinaDaLista);
 			}
 			JOptionPane.showMessageDialog(null, aluno.getAlunoAprovado());
-
-		} catch (Exception e) {
-			//uma função do java para trabalhar com string
+		} catch (NumberFormatException e) {
 			StringBuilder saidaBuilder = new StringBuilder();
-			
-			//imprime o erro no console
-			e.printStackTrace();
-			//um for para varrer o array e pegar os erros apresentados informando-os em uma mensagem personalizada
-			//este jeito é mais simples
 			for(int pos = 0; pos < e.getStackTrace().length; pos++) {
-				JOptionPane.showMessageDialog(null, "Não foi possivel salvar a disciplina e a nota. CAUSA: "+e.getMessage()
-													+"\n"+ "Linha do erro: "+ e.getStackTrace()[pos].getLineNumber()
-													+"\n"+"Metodo do erro: "+ e.getStackTrace()[pos].getMethodName()
-													+"\n"+"Classe do erro: "+e.getStackTrace()[pos].getClassName()
-													+"\n"+"Classe de erro: "+e.getClass().getName());
-													
-			//adiciona os erros ao saidaBuilder para que sejam chamados em uma futura impressão	
 			saidaBuilder.append("Não foi possivel salvar a disciplina e a nota. CAUSA: "+e.getMessage());
 			saidaBuilder.append("\n Linha do erro: "+ e.getStackTrace()[pos].getLineNumber());
 			saidaBuilder.append("\n Metodo do erro: "+ e.getStackTrace()[pos].getMethodName());
 			saidaBuilder.append("\n Classe do erro: "+e.getStackTrace()[pos].getClassName());
 			saidaBuilder.append("\n Classe de erro: "+e.getClass().getName());
-			
 			}
-			//imprime os erros adicionados ao saidaBuilder, este jeito é um pouco mais elaborado
-			JOptionPane.showMessageDialog(null, saidaBuilder.toString());
-
+			JOptionPane.showMessageDialog(null, saidaBuilder.toString()); 
+			}catch(NullPointerException e) {
+			JOptionPane.showMessageDialog(null, "Não foi possivel salvar os dados - CAUSA: " + e.getMessage());
 		}
 	}
 }
---
+++ AulasAvancadas/src/exemplosExcecao/ExemplosTryCatch.java
@@ -0,0 +1,48 @@
+package exemplosExcecao;
+
+import javax.swing.JOptionPane;
+
+public class ExemplosTryCatch {
+	public static void main(String[] args) {
+		try {
+			Aluno aluno = new Aluno();
+			for (int pos = 1; pos <= 2; pos++) {
+				String nomeDisciplina = JOptionPane.showInputDialog("Informe a " + pos + "ª disciplina:");
+				String notaDisciplina = JOptionPane
+						.showInputDialog("Informe a nota em " + nomeDisciplina + " do aluno: ");
+				ObjetoDisciplinaComLista discilplinaDaLista = new ObjetoDisciplinaComLista();
+				discilplinaDaLista.setDisciplina(nomeDisciplina);
+				discilplinaDaLista.setNota(Double.parseDouble(notaDisciplina));
+				aluno.getDisciplinas().add(discilplinaDaLista);
+			}
+			JOptionPane.showMessageDialog(null, aluno.getAlunoAprovado());
+
+		} catch (Exception e) {
+			//uma função do java para trabalhar com string
+			StringBuilder saidaBuilder = new StringBuilder();
+			
+			//imprime o erro no console
+			e.printStackTrace();
+			//um for para varrer o array e pegar os erros apresentados informando-os em uma mensagem personalizada
+			//este jeito é mais simples
+			for(int pos = 0; pos < e.getStackTrace().length; pos++) {
+				JOptionPane.showMessageDialog(null, "Não foi possivel salvar a disciplina e a nota. CAUSA: "+e.getMessage()
+													+"\n"+ "Linha do erro: "+ e.getStackTrace()[pos].getLineNumber()
+													+"\n"+"Metodo do erro: "+ e.getStackTrace()[pos].getMethodName()
+													+"\n"+"Classe do erro: "+e.getStackTrace()[pos].getClassName()
+													+"\n"+"Classe de erro: "+e.getClass().getName());
+													
+			//adiciona os erros ao saidaBuilder para que sejam chamados em uma futura impressão	
+			saidaBuilder.append("Não foi possivel salvar a disciplina e a nota. CAUSA: "+e.getMessage());
+			saidaBuilder.append("\n Linha do erro: "+ e.getStackTrace()[pos].getLineNumber());
+			saidaBuilder.append("\n Metodo do erro: "+ e.getStackTrace()[pos].getMethodName());
+			saidaBuilder.append("\n Classe do erro: "+e.getStackTrace()[pos].getClassName());
+			saidaBuilder.append("\n Classe de erro: "+e.getClass().getName());
+			
+			}
+			//imprime os erros adicionados ao saidaBuilder, este jeito é um pouco mais elaborado
+			JOptionPane.showMessageDialog(null, saidaBuilder.toString());
+
+		}
+	}
+}
