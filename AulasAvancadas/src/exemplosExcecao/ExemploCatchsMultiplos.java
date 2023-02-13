8a718b713131a8477278978d98424bc8077e90fb
Arthur Benfica <Arthur Benfica@PC-ARTHUR>
2023-02-13 14:24:10 -0300

    Criados mais alguns exemplos de exceção para meios de aprendizado.

--- AulasAvancadas/src/exemplosExcecao/ExemploCatchsMultiplos.java
+++ AulasAvancadas/src/exemplosExcecao/ExemploCatchsMultiplos.java
@@ -1,11 +1,17 @@
 package exemplosExcecao;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 import javax.swing.JOptionPane;
 
 public class ExemploCatchsMultiplos {
 	public static void main(String[] args) {
 		try {
 			Aluno aluno = new Aluno();
+			File fil = new File("Files.txt");
+			Scanner imagemScanner = new Scanner(fil);
 			String sexString = JOptionPane.showInputDialog("Selecione o sexo do aluno (M/F):");
 			aluno.setSexo(sexString);
 			String nomeString = JOptionPane.showInputDialog("Informe o nome do aluno:");
@@ -32,6 +38,8 @@ public class ExemploCatchsMultiplos {
 			JOptionPane.showMessageDialog(null, saidaBuilder.toString()); 
 			}catch(NullPointerException e) {
 			JOptionPane.showMessageDialog(null, "Não foi possivel salvar os dados - CAUSA: " + e.getMessage());
+		}catch(FileNotFoundException e) {
+			JOptionPane.showMessageDialog(null, "Não foi possivel encontrar a imagem - CAUSA: " + e.getMessage());
 		}
 	}
 }
