e515de578191e438e6592c83d082ca9f5c6a3933
Arthur Benfica <Arthur Benfica@PC-ARTHUR>
2023-02-16 19:11:24 -0300

    Criada a classe StringSplit que trabalha usando Split para pegar valores e criar campos em um Array e foram feitos exemplos de conversão de array em lista e de lista em array na mesma;

--- AulasAvancadas/src/matrizes/StringSplit.java
+++ AulasAvancadas/src/matrizes/StringSplit.java
@@ -1,13 +1,24 @@
 package matrizes;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class StringSplit {
 	public static void main(String[] args) {
-	 String texto = "Arthur, curso java, nota 80, nota 70, nota 89";
-	 String[] valoresArray = texto.split(","); // aqui cria o padrão que será usado para repartir o valor informado na string
-	 System.out.println("Nome do aluno " + valoresArray[0]);
-	 System.out.println("Curso aluno " + valoresArray[1]);
-	 System.out.println("nota 1 do aluno " + valoresArray[2]);
-	 System.out.println("nota 2 do aluno " + valoresArray[3]);
-	 System.out.println("nota 3 do aluno " + valoresArray[4]);
+	 String texto = "Arthur,curso java,nota 80,nota 70,nota 89";
+	 String[] valoresArray = texto.split(","); // aqui cria o padrão que será usado para repartir o valor informado na string, no caso é a virgula
+ 
+	 
+	 //convertendo um Array para uma lista
+	 List<String> list = Arrays.asList(valoresArray);
+	 for (String Valorstring : list) {
+		System.out.println(Valorstring);
+	}
+	 
+	 //convertendo uma lista para um Array             ↓ aqui ele busca o length do valoresArray
+	 String[] conversaoArray = list.toArray(new String[valoresArray.length]);
+	 for(int pos = 0; pos < conversaoArray.length; pos++) {
+		 System.out.println(conversaoArray[pos]);	 
+	 }
 	}
 }
