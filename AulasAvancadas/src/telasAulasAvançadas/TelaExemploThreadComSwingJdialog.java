241a07cf9e290e1ef0079d670758cf8a6e133ea2
Arthur Benfica <Arthur Benfica@PC-ARTHUR>
2023-02-18 19:40:26 -0300

    Criado o pacote telasAulasAvançadas que irá conter as telas dos exemplos criados nas proximas aulas.

---
+++ AulasAvancadas/src/telasAulasAvançadas/TelaExemploThreadComSwingJdialog.java
@@ -0,0 +1,11 @@
+package telasAulasAvançadas;
+
+import javax.swing.JDialog;
+
+public class TelaExemploThreadComSwingJdialog extends JDialog{
+	public TelaExemploThreadComSwingJdialog() {//executa o que tiver dentro no momento dá abertura ou execução
+		setVisible(true);
+		
+	}
+
+}
