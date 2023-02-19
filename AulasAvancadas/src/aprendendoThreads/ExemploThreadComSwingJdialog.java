2a204b5b3763ef02f6ab5276ead5b349d628e3e8
Arthur Benfica <Arthur Benfica@PC-ARTHUR>
2023-02-18 21:10:09 -0300

    Adicionadas algumas configurações a classe responsavel pela tela do primeiro exemplo.

---
+++ AulasAvancadas/src/aprendendoThreads/ExemploThreadComSwingJdialog.java
@@ -0,0 +1,10 @@
+package aprendendoThreads;
+
+import telasAulasAvançadas.TelaExemploThreadComSwingJdialog;
+
+public class ExemploThreadComSwingJdialog {
+	public static void main(String[] args) {
+		//↓ este objeto instanciado chama a tela que foi criada.
+		TelaExemploThreadComSwingJdialog tela = new TelaExemploThreadComSwingJdialog();
+	}
+}
